5861d581477c00a74718812c628f68f8e4f00395
ThePalad1n <31965502+ThePalad1n@users.noreply.github.com>
2020-03-03 00:22:02 -0500

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,18 +5,41 @@
     var losses = 0;
     var previousGuesses = [];
 
-    var userChoiceText = document.getElementById("userchoice-text");
-    var alphaChoiceText = document.getElementById("alphabet-text");
-    var winsText = document.getElementById("wins-text");
-    var losesText = document.getElementById("loses-text");
-    var guessesText = document.getElementById("guesses-text");
+    /*
+    var userChoiceText = document.getElementById("userchoice");
+    var alphaChoiceText = document.getElementById("alpha");
+    var winsText = document.getElementById("wins");
+    var losesText = document.getElementById("losses");
+    var guessesText = document.getElementById("guessesRemaining");
+*/
+ //When key is released it becomes the users guess
+function startGame(){
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        if (guessesRemaining > 0) {
+            if (userGuess == alphaSelect.alphaChoice) {
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                userGuess = userGuess.toUpperCase();
+                resetGame();
+            }
+        } 
+        else if (guessesRemaining == 0) {
+            // Then we will loss and we'll update the html to display the loss 
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            resetGame();
+        }
+        return false;
+    };
+}   
 
 function ugr() {
     // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
     document.querySelector('#guessRemaining').innerHTML = "Guesses remaining: " + guessesRemaining;
 };
 
-function alphaChoice() {
+function alphaSelect() {
     this.alphaChoice = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
 };
 
@@ -36,40 +59,5 @@ function resetGame(){
     startGame();
 };
 
-//When key is released it becomes the users guess
-document.onkeyup = function(event) {
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    var isIn = alphaChoice.includes(userGuess);
-    var isInn = previousGuesses.includes(userGuess);
-    // console.log(checkDouble);
-    if (isInn === true){
-        console.log("double letter");
-        alert("That letter has already been guessed, would you try again?");
-        return false;
-    } 
-    else if (isIn === true) {
-        //If the Users choice was an alphabet char then update guesses left and add users guess to the array of guessed letters
-        guessesLeft--;
-        guessedLetters.push(userGuess);
-        ugr();
-        upg();
 
-        if (guessesRemaining > 0) {
-            if (userGuess == alphaChoice) {
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                userGuess = userGuess.toUpperCase();
-                reset();
-            }
-        } else if (guessesRemaining == 0) {
-            // Then we will loss and we'll update the html to display the loss 
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            reset();
-        }
-        return false;
-        } 
-    else {
-        alert("error");
-    }
-};
+startGame();
